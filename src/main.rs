5df7421a75c4416b008252ca4702e16b5e92b0f4
jochem pouwels <jjpouwels2004@gmail.com>
2022-11-03 13:06:49 +0100

    playing certain frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use std::{thread::sleep, time::Duration, fmt};
+use std::{thread::sleep, time::Duration, fmt, f32::consts::PI};
 
-use cpal::{self, traits::{HostTrait, DeviceTrait, StreamTrait}, Sample, SampleFormat};
+use cpal::{self, traits::{HostTrait, DeviceTrait, StreamTrait}, Sample, SampleFormat, SampleRate};
+
+pub struct Note {
+    buffer: Vec<f32>,
+    index: usize
+}
+
+impl Note {
+    pub fn new(rate: SampleRate, frequency: f32) -> Self {
+        let mut buffer = Vec::new();
+        for i in 0..((rate.0 as f32 / frequency * 100.0) as u32) {
+            buffer.push((i as f32 / rate.0 as f32 * PI * 2.0 * frequency).sin());
+        }
+
+        println!("first: {:?}, last: {:?}", &buffer[0..5], &buffer[buffer.len() - 6 .. buffer.len() - 1]);
+
+        Self { buffer, index: 0 }
+    }
+
+    pub fn read_next(&mut self) -> f32 {
+        let value = self.buffer[self.index];
+        self.index += 1;
+        self.index %= self.buffer.len();
+        value
+    }
+}
 
 fn main() {
     let host = cpal::default_host();
@@ -13,13 +38,15 @@ fn main() {
     
     let sample_format = config.sample_format();
 
+    let mut note = Note::new(config.sample_rate(), 261.63);
+
     let err_fn = |err| eprintln!("Error in buffer: {}", err);
 
     let config = config.into();
     let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-        SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-        SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
+        SampleFormat::F32 => device.build_output_stream(&config, move |data, _| write_note::<f32>(&mut note, data), err_fn),
+        SampleFormat::I16 => device.build_output_stream(&config, move |data, _| write_note::<i16>(&mut note, data), err_fn),
+        SampleFormat::U16 => device.build_output_stream(&config, move |data, _| write_note::<u16>(&mut note, data), err_fn),
     }.unwrap();
 
     stream.play().unwrap();
@@ -27,8 +54,10 @@ fn main() {
     sleep(Duration::from_secs(5));
 }
 
-fn write_silence<T: Sample + fmt::Debug>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = Sample::from(&0.0);
+fn write_note<T: Sample + fmt::Debug>(note: &mut Note, data: &mut [T]) {
+    for sample in data.chunks_mut(2) {
+        let value = note.read_next();
+        sample[0] = Sample::from(&value);
+        sample[1] = Sample::from(&value);
     }
 }
