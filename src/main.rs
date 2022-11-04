a4fb39a9defbb4df09f82b5301bea42c7f0d06a5
jochem pouwels <jjpouwels2004@gmail.com>
2022-11-05 00:43:09 +0100

    playing pure tones of more precision

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::{thread::sleep, time::Duration, fmt, f32::consts::PI};
+use std::{thread::sleep, time::Duration, f32::consts::PI};
 
 use cpal::{self, traits::{HostTrait, DeviceTrait, StreamTrait}, Sample, SampleFormat, SampleRate};
+use playable::Playable;
 
+mod playable;
+
+#[derive(Debug, Clone)]
 pub struct Note {
     buffer: Vec<f32>,
     index: usize
@@ -14,16 +18,16 @@ impl Note {
             buffer.push((i as f32 / rate.0 as f32 * PI * 2.0 * frequency).sin());
         }
 
-        println!("first: {:?}, last: {:?}", &buffer[0..5], &buffer[buffer.len() - 6 .. buffer.len() - 1]);
-
         Self { buffer, index: 0 }
     }
+}
 
-    pub fn read_next(&mut self) -> f32 {
+impl Playable for Note {
+    fn read_next<T: Sample>(&mut self) -> T {
         let value = self.buffer[self.index];
         self.index += 1;
         self.index %= self.buffer.len();
-        value
+        Sample::from(&value)
     }
 }
 
@@ -44,20 +48,12 @@ fn main() {
 
     let config = config.into();
     let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(&config, move |data, _| write_note::<f32>(&mut note, data), err_fn),
-        SampleFormat::I16 => device.build_output_stream(&config, move |data, _| write_note::<i16>(&mut note, data), err_fn),
-        SampleFormat::U16 => device.build_output_stream(&config, move |data, _| write_note::<u16>(&mut note, data), err_fn),
+        SampleFormat::F32 => device.build_output_stream(&config, move |data, _| note.write_chunk::<f32>(data), err_fn),
+        SampleFormat::I16 => device.build_output_stream(&config, move |data, _| note.write_chunk::<i16>(data), err_fn),
+        SampleFormat::U16 => device.build_output_stream(&config, move |data, _| note.write_chunk::<u16>(data), err_fn),
     }.unwrap();
 
     stream.play().unwrap();
 
     sleep(Duration::from_secs(5));
-}
-
-fn write_note<T: Sample + fmt::Debug>(note: &mut Note, data: &mut [T]) {
-    for sample in data.chunks_mut(2) {
-        let value = note.read_next();
-        sample[0] = Sample::from(&value);
-        sample[1] = Sample::from(&value);
-    }
 }
