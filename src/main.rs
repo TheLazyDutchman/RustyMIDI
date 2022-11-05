6193c2e575f50f48f15946b5ae00ea086c85e130
jochem pouwels <jjpouwels2004@gmail.com>
2022-11-05 01:29:57 +0100

    started with rodio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::time::Duration;
+
+use rodio::{OutputStream, Sink, source::SineWave, Source};
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let note = SineWave::new(200.0).take_duration(Duration::from_secs(5));
+    sink.append(note);
+
+    sink.sleep_until_end()
 }
